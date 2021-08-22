d6e4e4d4f6f9cb1d2592d4d50388a32ecaa12db4
Mr Thinh <33323583+git-thinh@users.noreply.github.com>
2021-08-22 21:09:58 +0700

    obj

--- PDF2Image/Models/ImageMessage.cs
+++ PDF2Image/Models/ImageMessage.cs
@@ -3,10 +3,10 @@
     public class ImageMessage
     {
         public int id { set; get; }
-        public int total { set; get; }
         public int w { set; get; }
         public int h { set; get; }
-        public int quality { set; get; }
         public int size { set; get; }
+        public int quality { set; get; }
+        public int total { set; get; }
     }
 }
