30687c1fb12077796f07fb87be37f607a4366d32
Mr Thinh <33323583+git-thinh@users.noreply.github.com>
2021-09-18 10:12:46 +0700

    dsfsdf

--- Hangfire/Startup.cs
+++ Hangfire/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -26,11 +27,19 @@ namespace Hangfire
             services.AddHangfireServer();
 
             services.AddMvc();
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Swagger.Api", Version = "v1" });
+            });
+            services.AddControllers().AddApplicationPart(this.GetType().Assembly).AddControllersAsServices();
         }
 
         public void Configure(IApplicationBuilder app, IBackgroundJobClient backgroundJobs, IWebHostEnvironment env)
         {
             if (env.IsDevelopment()) app.UseDeveloperExceptionPage();
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "AIT.UI.Api v1"));
 
             backgroundJobs.Enqueue(() => Console.WriteLine("[1] - Hello world from Hangfire!"));
             BackgroundJob.Enqueue(() => Console.WriteLine("[2] - " + Guid.NewGuid().ToString()));
